da7fcb44643388b418d042ad0a4898aced27f53c
hlorenalopes <hlorenalopes@gmail.com>
2021-09-23 17:58:37 -0300

    feat: add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='container'>
+      <div className='row row-header'>
+        <div className='col col-header'>
+          <img src='logo-corebiz-global.svg' alt='Corebiz' />
+        </div>
+        <div className='col col-header'>
+          <nav className='navBar'>
+            <ul>
+              <li>
+                <a href='https://www.corebiz.ag/pt/about/'>a corebiz</a>
+                <div className='line'></div>
+              </li>
+              <li>
+                <a href='https://www.corebiz.ag/pt/#framework-title'>
+                  serviços
+                </a>
+              </li>
+              <li>
+                <a href='https://www.corebiz.ag/pt/cases/'>cases</a>
+              </li>
+              <li>
+                <a href='https://www.corebiz.ag/pt/contato/'>contato</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
