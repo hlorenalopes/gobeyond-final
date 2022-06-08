edcd0127c9643b1f889296c624158a5ab515b582
hlorenalopes <hlorenalopes@gmail.com>
2022-06-08 15:19:07 -0600

    fix images

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,28 +2,31 @@ import React, { useState, useEffect } from 'react';
 
 const Main = () => {
   const [activeContent, setActiveContent] = useState(0);
-  const [content, setContent] = useState([]);
+  const [photo, setPhoto] = useState([
+    { albumId: '', id: '', title: '', url: '', thumbnailUrl: '' },
+    { albumId: '', id: '', title: '', url: '', thumbnailUrl: '' },
+    { albumId: '', id: '', title: '', url: '', thumbnailUrl: '' },
+    { albumId: '', id: '', title: '', url: '', thumbnailUrl: '' },
+  ]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const getData = async () => {
       const res = await fetch(
         'https://jsonplaceholder.typicode.com/photos?_start=0&_limit=4',
       );
       const json = await res.json();
 
-      setContent(json);
+      setPhoto(json);
     };
 
-    fetchData();
+    getData();
   }, []);
 
   return (
     <main className='container'>
       <div className='row row-main'>
         <div className='col col-main col-main-left'>
-          {content[activeContent].map(({ title }, index) => (
-            <h1 key={index}>{title}</h1>
-          ))}
+          <h1>{photo[activeContent].title}</h1>
           <a className='btn-link' href='https://www.corebiz.ag/'>
             veja mais
             <img src='arrow-circle.svg' alt='Arrow Icon' />
@@ -34,39 +37,38 @@ const Main = () => {
               className={`${
                 activeContent === 0 ? 'tab-active' : 'tab-inactive'
               }`}
-              style={{ backgroundImage: `url${content[0].thumbnailUrl}` }}
+              style={{ backgroundImage: `url(${photo[0].thumbnailUrl})` }}
             ></button>
             <button
               onClick={() => setActiveContent(1)}
               className={`${
                 activeContent === 1 ? 'tab-active' : 'tab-inactive'
               }`}
-              style={{ backgroundImage: `url${content[1].thumbnailUrl}` }}
+              style={{ backgroundImage: `url(${photo[1].thumbnailUrl})` }}
             ></button>
             <button
               onClick={() => setActiveContent(2)}
               className={`${
                 activeContent === 2 ? 'tab-active' : 'tab-inactive'
               }`}
-              style={{ backgroundImage: `url${content[2].thumbnailUrl}` }}
+              style={{
+                backgroundImage: `url(${photo[2].thumbnailUrl})`,
+              }}
             ></button>
             <button
               onClick={() => setActiveContent(3)}
               className={`${
                 activeContent === 3 ? 'tab-active' : 'tab-inactive'
               }`}
-              style={{ backgroundImage: `url${content[3].thumbnailUrl}` }}
+              style={{ backgroundImage: `url(${photo[3].thumbnailUrl})` }}
             ></button>
           </div>
         </div>
         <div className='col col-main col-main-right'>
-          {content[activeContent].map(({ url }, index) => (
-            <div
-              key={index}
-              className='bigImg'
-              style={{ backgroundImage: `url(${url})` }}
-            ></div>
-          ))}
+          <div
+            className='bigImg'
+            style={{ backgroundImage: `url(${photo[activeContent].url})` }}
+          ></div>
         </div>
       </div>
     </main>
