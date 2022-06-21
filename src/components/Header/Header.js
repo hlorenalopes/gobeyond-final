3470d2e4cfc6cad9d4952f547826c90b7ce8bae4
hlorenalopes <hlorenalopes@gmail.com>
2022-06-21 15:02:23 -0600

    fix header responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,30 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Header = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const handleToggle = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', changeWidth);
+
+    return () => {
+      window.removeEventListener('resize', changeWidth);
+    };
+  }, []);
+
   return (
-    <header className='container'>
+    <header className='container containerHeader'>
       <div className='row row-header'>
-        <div className='col col-header'>
-          <img src='logo-corebiz-global.svg' alt='Corebiz' />
+        <div className='col'>
+          <img
+            className='logoHeader'
+            src='logo-corebiz-global.svg'
+            alt='Corebiz'
+          />
+          {screenWidth <= 768 && (
+            <button className='toggleMenu' onClick={handleToggle}>
+              <div className='toggleBar'></div>
+              <div className='toggleBar'></div>
+              <div className='toggleBar'></div>
+            </button>
+          )}
         </div>
-        <div className='col col-header'>
+
+        <div className='col colNav'>
           <nav className='navBar'>
-            <ul>
-              <li>
-                <a href='https://www.corebiz.ag/pt/about/'>a corebiz</a>
-              </li>
-              <li>
-                <a href='https://www.corebiz.ag/pt/#framework-title'>
-                  serviços
-                </a>
-              </li>
-              <li>
-                <a href='https://www.corebiz.ag/pt/cases/'>cases</a>
-              </li>
-              <li>
-                <a href='https://www.corebiz.ag/pt/contato/'>contato</a>
-              </li>
-            </ul>
+            {(toggleMenu || screenWidth > 768) && (
+              <ul>
+                <li>
+                  <a href='https://www.corebiz.ag/pt/about/'>a corebiz</a>
+                </li>
+                <li>
+                  <a href='https://www.corebiz.ag/pt/#framework-title'>
+                    serviços
+                  </a>
+                </li>
+                <li>
+                  <a href='https://www.corebiz.ag/pt/cases/'>cases</a>
+                </li>
+                <li>
+                  <a href='https://www.corebiz.ag/pt/contato/'>contato</a>
+                </li>
+              </ul>
+            )}
           </nav>
         </div>
       </div>
